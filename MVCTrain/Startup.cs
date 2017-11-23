827407f955d3dac7d215366afa16629681a614a1
Hephaestus901 <dima.emos@gmail.com>
2017-11-24 00:58:33 +0300

    Add project files.

---
+++ MVCTrain/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCTrain.Startup))]
+namespace MVCTrain
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
