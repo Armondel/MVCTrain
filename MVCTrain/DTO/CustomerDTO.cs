8ccc5454d0cbf084047392863e2d0c5f69820d0d
Hephaestus901 <dima.emos@gmail.com>
2017-12-03 21:26:38 +0300

    Added API for Movies and Customers.

---
+++ MVCTrain/DTO/CustomerDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using MVCTrain.Models;
+using MVCTrain.Models.Validation;
+
+namespace MVCTrain.DTO
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public DateTime? BirthdayDate { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+    }
+}
---
+++ MVCTrain/DTO/MovieDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using MVCTrain.Models;
+
+namespace MVCTrain.DTO
+{
+    public class MovieDto
+    {
+        public byte Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public byte? GenreId { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        [Range(1, 20)]
+        public byte NumberInStock { get; set; }
+    }
+}
